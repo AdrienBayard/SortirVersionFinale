77f4f6084bd6929a4e8a2acb72045124d018d2fc
AdrienEni <adrien.bayard2021@campus-eni.fr>
2022-02-09 12:23:50 +0100

    Correction AppAuthenticator.php

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -50,8 +50,8 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('sortie_index'));
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
