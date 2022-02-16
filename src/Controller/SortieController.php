aec646d2d6b8af4b44b16f521065c86a97c0cb36
adrien <adrien.bayard2021@campus-eni.fr>
2022-02-16 17:31:45 +0100

    adrien commit tri affichage sortie par dates

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -194,17 +194,43 @@ dump($this->getUser()->getUserIdentifier());
     }
 
     #[Route('/inscription/{id}', name: 'sortie_inscription')]
-    public function add_participant(EntityManagerInterface $em, Request $request, Sortie $sortie,int $id, SortieRepository $sortieRepository){
+    public function add_participant(EntityManagerInterface $entityManager, Request $request, Sortie $sortie,int $id, SortieRepository $sortieRepository, UserRepository $userRepository, User $user){
 
-        $sortie = $em->getRepository(Sortie::class)->find($id);
-        $sortie->addAEteInscrit($this->getUser());
+        // permet de récuperer le pseudo de la personne identifiée sur la page
+        $nouvelInscrit=$this->getUser()->getUserIdentifier();
+        dump($nouvelInscrit);
 
-        $em->persist($sortie);
-        $em->flush();
-        $this->addFlash('success', 'Tu es maintenant inscrit !');
+        // permet de récuperer le pseudo de l'organisateur dans la sortie
 
-        // return $this->render('sortie/show.html.twig',['id' => $id, 'sortie' => $sortieRepository->find($id)]);
-        return $this->redirectToRoute('sortie_show', ['id'=>$sortie->getId()]);
+       // $inscrit = $entityManager->getRepository(Sortie::class)->find($id);
+        //$inscritRecupere=$inscrit->getOrganisateur();
+        //dump($inscritRecupere);
+
+
+        //$sorties = $sortie->getAEteInscrit();
+
+
+        if($inscritRecupere != $nouvelInscrit){
+
+            $sortie = $entityManager->getRepository(Sortie::class)->find($id);
+            $sortie->addAEteInscrit($this->getUser());
+            $compteurInscrit=$sortie->getCompteur();
+            $compteurInscrit= $compteurInscrit+1;
+            $sortie->setCompteur($compteurInscrit);
+
+            $entityManager->persist($sortie);
+            $entityManager->flush();
+            $this->addFlash('success', 'Tu es maintenant inscrit !');
+
+            // return $this->render('sortie/show.html.twig',['id' => $id, 'sortie' => $sortieRepository->find($id)]);
+            return $this->redirectToRoute('sortie_show', ['id'=>$sortie->getId()]);
+
+}
+        else{
+            $this->addFlash('warning', "Vous êtes déjà inscrit à cet évènement!");
+            return $this->render('sortie/show.html.twig', ['sortie' => $sortie, 'sorties' => $sorties
+            ]);
+        }
 
 
 
